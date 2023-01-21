e0901d2c9c44621c43af47f8d035020a1701d5e3
EthanHuh <work.hethan@gmail.com>
2023-01-21 14:20:03 +0900

    -Add Login

---
+++ lib/login.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => LoginState();
+}
+
+class LoginState extends State<Login> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          child: Column(
+            children: [
+              Container(
+                
+              ),
+              
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
